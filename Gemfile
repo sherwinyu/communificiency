80c4d9ae8309af75018f9efead0ddd18ff240664
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-11 19:13:23 -0400

    added root

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jquery-rails', '2.0.0'
 
 
 gem 'rails', '3.2.5'
+gem 'bootstrap-sass', '2.0.0'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Communificiency::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
+
+  root :to => 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
