2c48adf0ce7fcfca7f8ab25839cb6bb01a9ba867
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-11 17:57:52 -0400

    static pages work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
-
 class StaticPagesController < ApplicationController
   def home
+    @title = 'Communificiency'
   end
 
   def help
+    @title = 'Help'
+  end
+
+  def about
+    @title = 'About'
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,29 +1,51 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  describe "Home Page" do
-    it "should have the content 'communificiency'" do
+  describe "Home page" do
+
+    it "should have the h1 'Communificiency'" do
       visit '/static_pages/home'
-      page.should have_content('communificiency')
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get static_pages_index_path
-      # response.status.should be(200)
+      page.should have_selector('h1', :text => 'Communificiency')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Communificiency")
+    end
+
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '|')
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Communificiency | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Communificiency'" do
+
+    it "should have the h1 'About'" do
       visit '/static_pages/about'
-      page.should have_content('About Communificiency') 
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Communificiency | About")
     end
   end
-
 end
