657934d1460b6e43051ab79e342394d1339cee38
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-11 19:14:54 -0400

    removed public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Communificiency::Application.routes.draw do
+  root :to => 'static_pages#home'
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
 
-  root :to => 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
