34dcfaa179dfee9d55007d8a38f878fcc6a00faa
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-11 14:21:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Communificiency::Application.config.secret_token = '5f58a87a57d72ab92032dda0981689c46d6b7e609e79ae69cf6643b525c7d3179dc9c0b1fc9d611d2bd6362d31df21c68aa111b21151c0a84799aa6f5c6f38e0'
