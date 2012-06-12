eb385a7dc8c82f6be027bf23b7f97e3051a8b205
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-12 02:02:09 -0400

    updated to use match routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'Communificiency'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Communificiency')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "Communificiency")
     end
 
     it "should not have a custom title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', text: '|')
     end
   end
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "Communificiency | Help")
     end
@@ -38,14 +38,27 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the h1 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path 
       page.should have_selector('title',
                     :text => "Communificiency | About")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    :text => "Communificiency | Contact")
     end
   end
 end
