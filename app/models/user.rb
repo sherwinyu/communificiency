0c63d6910e61f2658a129386aa60dd8b4e7005bd
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-28 19:38:56 -0400

    Add users model and tests

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_presence_of :email, :name
+  validates :email, format: {with: email_regex}
+  validates :email, uniqueness: {case_sensitive: false} 
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe User do
+  before :each do
+    @user_attr = { name: 'Namey User', email: 'namey.user@email.com' }
+  end
+
+  it "should accept a valid User" do
+    valid_user = User.new @user_attr
+    valid_user.should be_valid
+  end
+
+  it "should require a name" do
+    nameless_user = User.new @user_attr.merge({ name: "" })
+    nameless_user.should_not be_valid
+  end
+
+  it "should require an email" do
+    emailless_user = User.new @user_attr.merge({ email: "" })
+    emailless_user.should_not be_valid
+  end
+
+  it "should accept an user with a valid email" do
+    valid_emails = %w[sherwinyu@google.com sherwin.yu@google.com harry@k12.fcps.edu a.b@c.e lilgirl445@aaa.tl]
+    valid_emails.each do |e|
+      valid_user = User.new @user_attr.merge({ email: e })
+      valid_user.should be_valid
+    end
+  end
+  
+  it "should reject an user with an invalid email" do
+    invalid_emails = %w[4sherwinyugoogle.com arf @@ a.b.c @a.b.c]
+    invalid_emails.each do |e|
+      invalid_user = User.new @user_attr.merge({ email: e })
+      invalid_user.should_not be_valid
+    end
+  end
+
+  it "should reject an user with a duplicate email" do
+    User.create! @user_attr
+    duplicate_email_user = User.new @user_attr.merge({ name: "Bob" })
+    duplicate_email_user.should_not be_valid
+  end
+  
+  it "should reject an user with a duplicate email up to case" do
+    u = User.create! @user_attr
+    upcase_email_user = User.new @user_attr.merge({ name: "Bob",
+                                                    email: u.email.upcase })
+    upcase_email_user.should_not be_valid
+  end
+  
+end
