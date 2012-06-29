d16265d1aefe9c9195efb2e2f44a54c9b24b9d4b
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-29 18:55:20 -0400

    Fix error for mass assignment (password typo pasword) in user sign-up Tests pass for user successful sign up Flash looks nicer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    render layout: "main_with_flash"
   end
 
   def create
     redirect_to "/sign_up", layout: "main_with_flash" and return if params[:user].nil?
 
-    @user = User.new  name: params[:user][:name], email: params[:user][:email] 
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation] 
-
+    #@user = User.new  name: params[:user][:name], email: params[:user][:email] 
+    @user = User.new  params[:user]
+    #@user.password = params[:user][:password]
+    #@user.password_confirmation = params[:user][:password_confirmation] 
     if @user.save
-      flash.notice = "User successfully created!"
-      redirect_to "/projects/1", layout: "main_with_flash"
+      flash.notice = "Your profile was successfully created. Welcome to Communificiency!"
+      redirect_to @user, layout: "main_with_flash"
     else
-      flash.alert = "There were #{@user.errors.size} #{'errors'.pluralize(@user.errors.size)}"
       render 'sign_up', layout: "main_with_flash"
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,17 +12,19 @@ describe UsersController do
   end
 
   describe 'POST users#create' do
-    before :each do
-      @user_attr.merge!  name: '',                
-        email: '',     
-        password: 'p4ssw0rd',              
-        password_confirmation: 'invalidpasswordconfirmation' 
-    end
 
     describe 'failure' do
+
+      before :each do
+        @user_attr.merge!  name: '',                
+          email: '',     
+          password: 'p4ssw0rd',              
+          password_confirmation: 'invalidpasswordconfirmation' 
+      end
+
       it 'should render the sign up page' do
         post :create, user: @user_attr
-        should render_template 'users/sign_up'
+        response.should render_template 'users/sign_up'
       end
 
       it 'should not change the user count' do
@@ -31,6 +33,30 @@ describe UsersController do
         end.should_not change(User, :count)
       end
 
+    end
+
+    describe 'success' do
+      before :each do 
+        @user = User.new @user_attr
+        puts @user_attr
+      end
+
+      it 'should redirect to the profile page' do
+        post :create, user: @user_attr
+        #response.should render_template 'users/show', 
+        response.should redirect_to user_path(assigns(:user))
+      end
+
+      it 'should increase users by 1' do
+        lambda do
+          post :create, user: @user_attr
+        end.should change(User, :count).by(1)
+      end
+
+      it 'should display a flash notice' do
+        post :create, user: @user_attr
+        flash.notice.should =~ /your profile was successfully created/i
+      end
     end
 
     it 'should redirect a valid new signup to the profile page' do
