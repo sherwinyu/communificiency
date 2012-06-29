cf5c8c97d237fabecc4b1e0c66cc9b18b7335ce5
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-29 04:59:58 -0400

    Add styling to error forms on user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def sign_up
+    @user = User.new
+    render 'sign_up', layout: "main_with_flash"
   end
 
   def show
@@ -9,12 +11,15 @@ class UsersController < ApplicationController
   def create
     redirect_to "/sign_up", layout: "main_with_flash" and return if params[:user].nil?
 
-    u = User.new params[:user]
-    if u.save
+    @user = User.new  name: params[:user][:name], email: params[:user][:email] 
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation] 
+
+    if @user.save
       flash.notice = "User successfully created!"
       redirect_to "/projects/1", layout: "main_with_flash"
     else
-      flash.alert = "There were #{u.errors.size} #{'errors'.pluralize(u.errors.size)}"
+      flash.alert = "There were #{@user.errors.size} #{'errors'.pluralize(@user.errors.size)}"
       render 'sign_up', layout: "main_with_flash"
     end
   end
