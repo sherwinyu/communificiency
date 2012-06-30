ec8e623be46219eab28a6733eb5e2903ad90346b
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-06-29 21:01:11 -0400

    various changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,8 @@ class UsersController < ApplicationController
   def create
     redirect_to "/sign_up", layout: "main_with_flash" and return if params[:user].nil?
 
-    #@user = User.new  name: params[:user][:name], email: params[:user][:email] 
     @user = User.new  params[:user]
-    #@user.password = params[:user][:password]
-    #@user.password_confirmation = params[:user][:password_confirmation] 
+
     if @user.save
       flash.notice = "Your profile was successfully created. Welcome to Communificiency!"
       redirect_to @user, layout: "main_with_flash"
