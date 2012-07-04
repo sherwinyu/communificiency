09f95b691354ee52d4af1f27e77f6659abd2c7db
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-07-04 05:03:34 -0400

    add picture fileto project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120704024647) do
+ActiveRecord::Schema.define(:version => 20120704085303) do
 
   create_table "payments", :force => true do |t|
     t.float    "amount"
@@ -25,14 +25,15 @@ ActiveRecord::Schema.define(:version => 20120704024647) do
   end
 
   create_table "projects", :force => true do |t|
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
     t.string   "name"
-    t.string   "short_description"
-    t.string   "long_description"
+    t.text     "short_description", :limit => 1000
+    t.text     "long_description",  :limit => 255
     t.string   "video"
     t.float    "funding_needed"
     t.string   "proposer_name"
+    t.string   "picture"
   end
 
   create_table "users", :force => true do |t|
