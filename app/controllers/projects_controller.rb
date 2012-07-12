e0dc31b2662b6349cff1be15447b816e93ed2c7b
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-07-12 15:23:00 -0400

    massive design changes. flip color scheme to white on top, tone down boldness of rewards bar, add embellishments and drop shadows, clean up code

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,7 +29,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @contribution = @project.contributions.build
-    render layout: "main_without_flash"
+    # render layout: "main_without_flash"
   end
 
   # GET /projects/new
