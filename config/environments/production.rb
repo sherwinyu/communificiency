1f44151c29e1beee7621fedc8ef084622bd694b7
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-07-24 02:36:49 -0400

    force ssl in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Communificiency::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  # changed to true 2012 07 24 http://ruby.railstutorial.org/chapters/sign-up#sec:deploying_to_production_with_ssl
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
