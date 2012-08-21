a57a6ed5c84b09f22febee9f726a583d0a474067
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-08-21 19:08:31 -0400

    add keys to application.rb; removed from initializers/stripe.rb; change application.html.erb to reference config key instead of constant

--- config/application.rb
+++ config/application.rb
@@ -83,8 +83,12 @@ module Communificiency
 
     config.STRIPE_API_KEY_TEST = "sk_0D4ItJKUqqLPRZ3vS9A8Mgl6IiHMz" 
     config.STRIPE_API_KEY_TEST_PUBLIC = "pk_0D4IReQ9k27INskXO9QqIN8nh39bQ" 
+
     config.STRIPE_API_KEY_PROD = "sk_0D4IlAP7YQ4Q0uLT43PMe5JCJe9zl"
     config.STRIPE_API_KEY_PROD_PUBLIC = "pk_0D4ISXBTWRBQFdwygUwzrOXOc6xC4" 
+
+    config.STRIPE_API_KEY =  config.STRIPE_API_KEY_TEST
+    config.STRIPE_API_KEY_PUBLIC =  config.STRIPE_API_KEY_TEST_PUBLIC
 
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,10 @@
-Stripe.api_key = "sk_0D4ItJKUqqLPRZ3vS9A8Mgl6IiHMz"
+Stripe.api_key = Communificiency::Application.config.STRIPE_API_KEY
 
-STRIPE_API_KEY_TEST = "sk_0D4ItJKUqqLPRZ3vS9A8Mgl6IiHMz" 
-STRIPE_API_KEY_TEST_PUBLIC = "pk_0D4IReQ9k27INskXO9QqIN8nh39bQ" 
-STRIPE_API_KEY_PROD = "sk_0D4IlAP7YQ4Q0uLT43PMe5JCJe9zl"
-STRIPE_API_KEY_PROD_PUBLIC = "pk_0D4ISXBTWRBQFdwygUwzrOXOc6xC4" 
-#STRIPE_PUBLIC_KEY = "pk_0D4IReQ9k27INskXO9QqIN8nh39bQ "
-#
+# STRIPE_API_KEY_TEST = "sk_0D4ItJKUqqLPRZ3vS9A8Mgl6IiHMz" 
+# STRIPE_API_KEY_TEST_PUBLIC = "pk_0D4IReQ9k27INskXO9QqIN8nh39bQ" 
+# STRIPE_API_KEY_PROD = "sk_0D4IlAP7YQ4Q0uLT43PMe5JCJe9zl"
+# STRIPE_API_KEY_PROD_PUBLIC = "pk_0D4ISXBTWRBQFdwygUwzrOXOc6xC4" 
+
 StripeEvent.setup do
   subscribe 'charge.failed' do |event|
     p = Payment.find_by_stripe_charge_id event[:data][:object][:id]
