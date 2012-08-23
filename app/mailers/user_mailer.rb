9a3223377780f8c5832512b78577e8c2a6c6327f
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-08-23 19:54:29 -0400

    clean up contributions_controller, now sends contribution_confirmation email.Add contribution_confirmail action to UserMailer and associated views.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,24 @@
 class UserMailer < ActionMailer::Base
-  default from: "notifications@communificiency.com"
+  default from: "team@communificiency.com"
 
   def welcome_email(user)
     @user = user
     @url = sign_in_url host: "communificiency.com"
     mail(to: user.email,
          subject: "Welcome to Communificiency!")
-         # content_type: "text/html",
+    # content_type: "text/html",
+  end
+
+  def contribution_confirmation(contribution)
+    @contribution = contribution
+    @user = @contribution.user
+    email_with_name = "#{@user.name} <#{@user.email}>"
+    mail(to: email_with_name,
+         from: "Communificiency Team <team@communificiency.com>",
+         subject: "Communificiency contribution confirmation: #{@contribution.project.name}") do |format|
+      format.text
+      format.html
+    end
   end
 
 
